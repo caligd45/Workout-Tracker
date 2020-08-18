f87918c13e91e0f24694d29e48d12d9258b0caae
Greg Doyle <caligd45@gmail.com>
2020-08-18 11:48:25 -0600

    trying to fix routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,17 +1,21 @@
-const express = require("express");
-const path = require("path");
-const router = express.Router();
+const path = require('path');
+const router = require('express').Router();
 
-router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(_dirname, "../public/exercise.html"));
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-router.get("/index", (req, res) => {
-    res.sendFile(path.join(_dirname, "../public/index.html"));
+router.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
 });
 
-router.get("/stats", (req, res) => {
-    res.sendFile(path.join(_dirname, "../public/stats.html"));
+router.get('/exercise?', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+});
+
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
 });
 
 module.exports = router;
